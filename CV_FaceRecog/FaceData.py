7e7c58aa45f1b5a946b766a2a948fe028cc5ca23
unknown <shubhamsinghal12@gmail.com>
2025-03-07 12:42:52 +0530

    CV

--- CV_FaceRecog/FaceData.py
+++ CV_FaceRecog/FaceData.py
@@ -27,7 +27,7 @@ while True:
 	if ret==False:
 		continue
 
-	# gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	
 
 	faces = face_cascade.detectMultiScale(frame,1.3,5)
@@ -37,20 +37,19 @@ while True:
 	faces = sorted(faces,key=lambda f:f[2]*f[3])
 
 	# Pick the last face (because it is the largest face acc to area(f[2]*f[3]))
-    
-    x,y,w,h = faces[-1]
-    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+	for face in faces[-1:]:
+		x,y,w,h = face
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
 
-    #Extract (Crop out the required face) : Region of Interest
-    offset = 10
-    face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
-    face_section = cv2.resize(face_section,(100,100))
+		#Extract (Crop out the required face) : Region of Interest
+		offset = 10
+		face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+		face_section = cv2.resize(face_section,(100,100))
 
-    skip += 1
-    if skip%10==0:
-        face_data.append(face_section)
-        print(len(face_data))
-
+		skip += 1
+		if skip%10==0:
+			face_data.append(face_section)
+			print(len(face_data))
 
 	cv2.imshow("Frame",frame)
 	cv2.imshow("Face Section",face_section)
@@ -61,6 +60,7 @@ while True:
 
 # Convert our face list array into a numpy array
 face_data = np.asarray(face_data)
+print(face_data.shape)
 face_data = face_data.reshape((face_data.shape[0],-1))
 print(face_data.shape)
 
